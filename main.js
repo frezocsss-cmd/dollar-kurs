dfe256151d49d5e6a1742ef8fe542e271a71be33
Rasul Ravshanbekov <frezocsss@gmail.com>
2026-03-02 07:03:59 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// let ism = prompt("Ismingizni kiriting")
+// let dollar = alert("1$ = 12500 som");
+// let summa = prompt("Ozbek somida summa kiriting")
+// alert(`Hurmatli ${ism} siz kiritgan ${summa}$ = ${summa * 12500} somga teng`)
+
+
+// console.log(ism, dollar, summa,);
+
+
+
+
+// alert("Korzinkaga Xush kelibsiz")
+// let tovar = prompt("Sizga qanday ichimlik kerak ?")
+// alert(`Bizning dokonimzda eng kop sotib olinadigan ichimlik ${tovar} hisoblanadi`)
+// alert(`${tovar}ning narxi 10000 som`)
+// let son = Number (prompt(`Sizga ${tovar}dan nechta kere`))
+// let jami = son * 10000
+// let jb = confirm(`Siz rosatam ${tovar}dan ${son} ta olaszmi `)
+// alert(` Siz ${tovar} mahsulotidan ${son} sotib oldingiz \n Jami: ${jami} som boldi `)
+// alert("Xaridingiz uchun Rahmat ")
+
+
+
+
+
+
